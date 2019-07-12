5f1dde85b9a3e1d751408cc7a255611ccdd10766
Michał Gregorczyk <michalgr@fb.com>
2019-07-12 11:13:37 -0400

    improve setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,15 +4,34 @@ echo "setting up sysroot installed at $SYSROOT"
 
 # links below are required by bcc python library which opens those libs with
 # dlopen. Not the best solution but a solution
-if [ ! -f $SYSROOT/lib/libbcc.so.0 ]; then
+if [[ ! -e $SYSROOT/lib/libbcc.so.0 ]]; then
     ln $SYSROOT/lib/libbcc.so -s $SYSROOT/lib/libbcc.so.0
 fi
-if [ ! -f $SYSROOT/lib/libc.so.6 ]; then
+if [[ ! -e $SYSROOT/lib/libc.so.6 ]]; then
     ln /system/lib64/libc.so -s $SYSROOT/lib/libc.so.6
 fi
-if [ ! -f $SYSROOT/lib/librt.so.1 ]; then
+if [[ ! -e $SYSROOT/lib/librt.so.1 ]]; then
     ln /system/lib64/libc.so -s $SYSROOT/lib/librt.so.1
 fi
 
+# attempt to unpack kernel-headers if kernel-headers dir does not exist
+if [[ ! -e $SYSROOT/kernel-headers && \
+          -e $PACKED_KERNEL_HEADERS_PATH ]]; then
+    echo "unpacking kernel headers..."
+    ABSOLUTE_PACKED_HERNEL_HEADERS_PATH=$(realpath $PACKED_KERNEL_HEADERS_PATH)
+    (cd $SYSROOT && tar xf $ABSOLUTE_PACKED_HERNEL_HEADERS_PATH)
+fi
+
 export PATH=$SYSROOT/bin:$PATH
-export LD_LIBRARY_PATH=$SYSROOT/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$SYSROOT/lib:$SYSROOT/lib64:$LD_LIBRARY_PATH
+
+# define environment variables bpftrace and bcc need to determine arch and
+# kernel source path
+export ARCH="arm64"
+export BPFTRACE_KERNEL_SOURCE=$SYSROOT/kernel-headers
+export BCC_KERNEL_SOURCE=$SYSROOT/kernel-headers
+
+# tell python where to find bcc in case we built the package on ubuntu/debian
+export PYTHONPATH=$SYSROOT/lib/python3/dist-packages:$PYTHONPATH
+
+echo "done"
